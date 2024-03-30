063dc442fb27aa750b9f231f642948fcf9ff6dd2
jonggking <scpark6021@gmail.com>
2024-03-30 03:22:55 +0000

    [Fix]: proxy설정 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosResponse } from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API_URL,
+  // baseURL: import.meta.env.VITE_BASE_API_URL,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
   },
